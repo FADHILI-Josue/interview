fb12ad7022b1bc1388235c897cd93701d4155ecd
FADHILI-Josue <fadhilijosue27@gmail.com>
2023-10-15 22:30:15 +0200

    making the whole project

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { Translations } from "@/i18n/translations";
+import Link from "next/link";
+import { getLocale } from "nitlix-i18n"
+import { post } from "./posts/[id]/page";
+
+const pageTranslations: Translations = {
+    en: {
+        welcome: "Welcome!"
+    }
+}
+
+export default async function () {
+    const lang: any = getLocale();
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+    return <div className="w-screen">
+        <div className="w-full mx-5 grid grid-cols-1 sm:grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-4">
+            {posts.map((e: post) => <Link href={`/posts/${e.id}`}>
+                <div className="w-full prose border border-slate-700">
+                    <h1>{e.title}</h1>
+                    <p>{e.body}</p>
+                </div>
+            </Link>)}
+        </div>
+    </div>
+
+}
---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,26 @@
+export interface post {
+    userId: number,
+    id: number,
+    title: string,
+    body: string,
+
+}
+export async function generateStaticParams() {
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+
+    return posts.map((post: post) => ({
+        id: post.id.toString(),
+    }))
+}
+
+
+const page = async ({ params: { id }, }: { params: { id: number } }) => {
+    const posts:post[] = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+
+    return <div className=" prose">
+        <h1>{posts[id].title}</h1>
+        <p>{posts[id].body}</p>
+    </div>
+}
+
+export default page
